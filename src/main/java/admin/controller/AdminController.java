e1df31b061e0a7c7ffd62cdb5a0455255cd61531
xoxosmj <xoxosmj@gmail.com>
2024-11-26 12:35:26 +0900

    [BE] Ref: 관리자, 댓글 패키지 logger 추가

--- src/main/java/admin/controller/AdminController.java
+++ src/main/java/admin/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @Slf4j
 @RequestMapping("/admin")
 public class AdminController {
