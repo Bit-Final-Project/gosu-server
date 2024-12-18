efb43e3c6f4470295a3610f0e07caf435fd07528
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2024-12-18 11:07:44 +0900

    [BE] Feat: 리뷰 조회 로직 구현 (#179)

--- src/main/java/com/ncp/moeego/review/bean/ReviewDTO.java
+++ src/main/java/com/ncp/moeego/review/bean/ReviewDTO.java
@@ -13,29 +13,35 @@ import lombok.Setter;
 @Setter
 public class ReviewDTO {
     private Long reviewNo;             // 리뷰 번호
-    private Long proItemNo;            // ProItem의 ID
-    private Long mainCateNo;       // MainCategory의 ID
-    private Long memberNo;             // Member의 ID
     private String reviewContent;      // 리뷰 내용
     private float star;                // 별점 (0~5.0)
     private LocalDateTime writeDate;   // 작성일
+    private String proName;       // 달인 이름
+    private String subject;       // 서비스 이름
+    private String memberName;    // 리뷰 작성자
+    private String elapsedTime;  // 경과 시간 (서비스 계층에서 계산)
+    private Long proItemNo;            // ProItem의 ID
+    private Long mainCateNo;       // MainCategory의 ID
+    private Long memberNo;             // Member의 ID
     private List<MultipartFile> imageFiles;
 
+    
     // 기본 생성자
     public ReviewDTO() {
     	
     }
-
-    // 생성자
-    public ReviewDTO(Long reviewNo, Long proItemNo, Long mainCateNo, Long memberNo, 
-                     String reviewContent, float star, LocalDateTime writeDate, List<MultipartFile> imageFiles) {
+    
+    // 전체 리뷰 조회 생성자
+    public ReviewDTO(Long reviewNo, String proName,float star, String subject, String reviewContent, String memberName, LocalDateTime writeDate, String elapsedTime) {
         this.reviewNo = reviewNo;
-        this.proItemNo = proItemNo;
-        this.mainCateNo = mainCateNo;
-        this.memberNo = memberNo;
-        this.reviewContent = reviewContent;
+    	this.proName = proName;
         this.star = star;
+    	this.subject = subject;
+        this.reviewContent = reviewContent;
+        this.memberName = memberName;
         this.writeDate = writeDate;
-        this.imageFiles = imageFiles;
+        this.elapsedTime = elapsedTime;
     }
+    
+    
 }
