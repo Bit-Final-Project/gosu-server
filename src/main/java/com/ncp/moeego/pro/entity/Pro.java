0ca18e372e33b27b136aed7a8608107ad54f1cd5
xoxosmj <xoxosmj@gmail.com>
2024-12-19 11:49:32 +0900

    [BE] Feat: Reservation, ReservationTime 엔티티 구현 #198

--- src/main/java/com/ncp/moeego/pro/entity/Pro.java
+++ src/main/java/com/ncp/moeego/pro/entity/Pro.java
@@ -2,7 +2,6 @@ package com.ncp.moeego.pro.entity;
 
 import com.ncp.moeego.category.entity.MainCategory;
 import com.ncp.moeego.member.entity.Member;
-import com.ncp.moeego.reserve.entity.Reserve;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -23,10 +22,6 @@ public class Pro {
     @ManyToOne // 고수는 하나의 메인 카테고리를 선택할수있음
     @JoinColumn(name = "main_cate_no", nullable = false)
     private MainCategory mainCategory;
-
-    @ManyToOne // 고수는 여러개의 예약신청을 받을수도 있고 예약을 못 받을수도있음
-    @JoinColumn(name = "reserve_no", nullable = true)
-    private Reserve reserve;
 
     @Column(name = "access_date")
     private LocalDateTime accessDate;
