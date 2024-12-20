7193f1326cbc8e4e021502b769ac84b6e3287766
xoxosmj <81194812+xoxosmj@users.noreply.github.com>
2024-12-20 10:10:47 +0900

    [BE] Fix: ReservationRequest dto클래스 수정 (#208)
    * [BE] Fix: 예약 기능 수정 #207
    
    * [BE] Fix: 예약 기능 수정 #207
    
    * [BE] Fix: 예약 기능 수정 #207

--- src/main/java/com/ncp/moeego/reservation/dto/ReservationRequest.java
+++ src/main/java/com/ncp/moeego/reservation/dto/ReservationRequest.java
@@ -1,7 +1,10 @@
 package com.ncp.moeego.reservation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,10 +12,16 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationRequest {
 
-    Long memberNo;
+    private Long memberNo;
     private Long proItemNo;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+
+    @JsonFormat(pattern = "HH:mm:ss")
     private List<LocalTime> startTimes;
 }
