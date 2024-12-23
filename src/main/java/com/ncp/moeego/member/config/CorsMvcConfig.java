866a343fd8997de82f31475ee28a08eb6dae8f9c
jaeheoon <aleh101@naver.com>
2024-12-23 15:33:15 +0900

    [BE] Ref: 배포를 위한 Cors 수정

--- src/main/java/com/ncp/moeego/member/config/CorsMvcConfig.java
+++ src/main/java/com/ncp/moeego/member/config/CorsMvcConfig.java
@@ -9,7 +9,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                    .allowedOrigins("*") // 모든 출처 허용
+                    .allowedOriginPatterns("*") // 모든 출처 허용 (패턴 기반)
                     .allowedMethods("*") // 모든 HTTP 메서드 허용
                     .allowedHeaders("*") // 모든 헤더 허용
                     .exposedHeaders("Set-Cookie") // 노출할 헤더
