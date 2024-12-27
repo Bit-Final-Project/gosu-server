779818ea2c965561ee65b64e66b4fd1f50efc9ad
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2024-12-27 20:30:49 +0900

    [BE] Ref: 카카오 로그인 배포 IP 수정

--- src/main/java/com/ncp/moeego/member/config/CorsMvcConfig.java
+++ src/main/java/com/ncp/moeego/member/config/CorsMvcConfig.java
@@ -9,7 +9,10 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://211.188.56.236:80") // 프론트 서버의 주소
+                .allowedOrigins(
+                    "http://211.188.56.236:80",     // 프론트 서버
+                    "http://211.188.48.106:8080"    // 백엔드 서버
+                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
