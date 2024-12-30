d214956ec32a53e00c82da79a3eafa1a502e3ff4
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2024-12-30 20:04:05 +0900

    [BE] Ref: CookieUtill 배포 후 로직 수정

--- src/main/java/com/ncp/moeego/member/util/CookieUtil.java
+++ src/main/java/com/ncp/moeego/member/util/CookieUtil.java
@@ -1,13 +1,12 @@
 package com.ncp.moeego.member.util;
 
-import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
-    public static Cookie createCookie(String key, String value, Integer expiredS) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(expiredS);
-        return cookie;
+    public static void createCookie(HttpServletResponse response, String key, String value, Integer expiredS) {
+        // SameSite=None을 사용하여 크로스 사이트 요청에서도 쿠키 전송
+        String cookieString = String.format("%s=%s; Path=/; Max-Age=%d; HttpOnly; Secure; SameSite=None", 
+                                            key, value, expiredS);
+        response.addHeader("Set-Cookie", cookieString);
     }
 }
