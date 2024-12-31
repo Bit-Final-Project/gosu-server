455f83d3cbb6deea6d7dc5d321870981cc23c27d
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2024-12-31 14:26:01 +0900

    [BE] Ref: CookieUtil.java 파일 원복

--- src/main/java/com/ncp/moeego/member/util/CookieUtil.java
+++ src/main/java/com/ncp/moeego/member/util/CookieUtil.java
@@ -1,14 +1,10 @@
 package com.ncp.moeego.member.util;
 
 import jakarta.servlet.http.Cookie;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 public class CookieUtil {
-
     public static Cookie createCookie(String key, String value, Integer expiredS) {
-        String encodedValue = URLEncoder.encode(value, StandardCharsets.UTF_8);
-        Cookie cookie = new Cookie(key, encodedValue);
+        Cookie cookie = new Cookie(key, value);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         cookie.setMaxAge(expiredS);
